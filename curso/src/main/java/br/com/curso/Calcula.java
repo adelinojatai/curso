6774c03bf4b7fb5b64705fb5898c4d015c88ce1d
Adelino Carvalho <adelino@audasi.com.br>
2015-11-11 06:45:10 -0200

    Alteração na calculadora

---
+++ curso/src/main/java/br/com/curso/Calcula.java
@@ -0,0 +1,30 @@
+package br.com.curso;
+
+import com.sun.xml.internal.ws.developer.Serialization;
+
+@Serialization
+public class Calcula {
+	Double result = 0.0;
+		
+	public Double somar(Double valueA, Double valueB){
+		result = valueA + valueB;
+		return result;
+	}
+	
+	public Double subtrair(Double valueA, Double valueB){
+		result = valueA - valueB;
+		return result;
+	}
+	
+	public Double multiplicar(Double valueA, Double valueB){
+		result = valueA * valueB;
+		return result;
+	}
+	
+	public Double dividir(Double valueA, Double valueB){
+		result = valueA / valueB;
+		return result;
+	}
+	
+	
+}
---
+++ curso/src/test/java/br/com/curso/CalculaTestes.java
@@ -0,0 +1,44 @@
+package br.com.curso;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculaTestes {
+
+	Calcula calcula;
+	Double result;
+	
+	@Test
+	public void testSomar() {
+		calcula = new Calcula();
+		result = calcula.somar(20.0, 10.0);
+		
+		assertEquals(Double.valueOf(30.0), result);
+	}
+
+	@Test
+	public void testSubtrair() {
+		calcula = new Calcula();
+		result = calcula.subtrair(20.0, 10.0);
+		
+		assertEquals(Double.valueOf(10.0), result);
+	}
+
+	@Test
+	public void testMultiplicar() {
+		calcula = new Calcula();
+		result = calcula.multiplicar(2.0, 10.0);
+		
+		assertEquals(Double.valueOf(20.0), result);
+	}
+
+	@Test
+	public void testDividir() {
+		calcula = new Calcula();
+		result = calcula.dividir(20.0, 10.0);
+		
+		assertEquals(Double.valueOf(2.0), result);
+	}
+
+}
